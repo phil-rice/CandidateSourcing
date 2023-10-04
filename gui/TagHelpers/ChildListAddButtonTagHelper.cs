aed78c59525ffba3e51581eed168443e316274fa
phil <phil.rice@validoc.org>
2023-10-04 15:01:13 +0200

    more tidying up for ChildList

---
+++ gui/TagHelpers/ChildListAddButtonTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+namespace gui.TagHelpers
+{
+
+
+    [HtmlTargetElement("child-list-add-button", Attributes = BindToAttributeName)]
+    public class ChildListAddButtonTagHelper : TagHelper
+    {
+        private const string BindToAttributeName = "bind-to";
+
+        [HtmlAttributeName(BindToAttributeName)]
+        public ModelExpression BindTo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            var buttonId = $"add.{BindTo.Name.ToLower()}.button";
+            output.TagName = "button";
+            output.Content.SetContent(output.GetChildContentAsync().Result.GetContent());
+            output.Attributes.SetAttribute("type", "button");
+            output.Attributes.SetAttribute("id", buttonId);
+        }
+    }
+
+}
