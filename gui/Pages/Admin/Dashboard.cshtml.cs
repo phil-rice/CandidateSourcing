77a483e4725db9e76118aad78b34b0d187e7e0dc
phil <phil.rice@validoc.org>
2023-10-05 08:54:40 +0200

    Admin and nav added

---
+++ gui/Pages/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gui.Pages.Admin
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
